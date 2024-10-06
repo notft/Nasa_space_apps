3fcb0d9dbf418fc651f36d7ea72458a47f4edfff
notft <aibelbinzacariah@gmail.com>
2024-10-06 08:47:46 +0530

    linked confetti and home

--- app/confetti/page.jsx
+++ app/confetti/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Confetti from 'react-confetti';
 import { useWindowSize } from 'react-use';
+import Link from 'next/link';
 
 export default function SuccessPage() {
   const [showConfetti, setShowConfetti] = useState(true);
@@ -33,13 +34,14 @@ export default function SuccessPage() {
       {/* Success message */}
       <div className="flex flex-col items-center gap-4 z-10">
         <div className="w-[500px] sm:w-[800px] h-[300px] bg-orange-400 flex flex-col justify-center items-center rounded-md text-center shadow-lg mx-auto my-10">
-          <h2 className="text-4xl font-bold">Order Successful</h2>
-          <p className="mt-10 text-2xl text-black">You successfully ordered! Enjoy feeding the needy!</p>
+          <h2 className="text-4xl font-bold">Adding Successful</h2>
+          <p className="mt-10 text-2xl text-black">You successfully Added! Thank you for feeding the needy!</p>
         </div>
 
-        <button className="bg-orange-500 text-white px-6 py-2 mt-8 rounded-md hover:bg-orange-600 w-full sm:w-auto transition duration-300">
-          CONTINUE
+        <button  className="bg-orange-500 text-white px-6 py-2 mt-8 rounded-md hover:bg-orange-600 w-full sm:w-auto transition duration-300">
+        <Link href='/home'>CONTINUE </Link>
         </button>
+        
       </div>
       </div>
   );
